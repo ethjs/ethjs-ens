e3e723d77ac97ba63820ca3b3cc3260295ac9788
Dan Finlay <dan@danfinlay.com>
2017-03-08 14:50:29 -0800

    Add sample page

---
+++ example/index.js
@@ -0,0 +1,30 @@
+const ENS = require('../')
+const HttpProvider = require('ethjs-provider-http')
+let ens
+
+// For MetaMask or Mist compatibility:
+window.addEventListener('load', function() {
+  if (typeof window.web3 !== 'undefined') {
+    ens = new ENS({ provider: web3.currentProvider, network: '3' })
+  } else {
+    const provider = new HttpProvider('https://ropsten.infura.io')
+    ens = new ENS({ provider, network: '3' })
+  }
+})
+
+searchButton.addEventListener('click', function() {
+  console.log('clicked button.')
+  var query = lookupField.value
+  console.log('querying for ' + query)
+  ens.lookup(query)
+  .then((address) => {
+    console.log('ens returned ' + address)
+    resultField.innerText = address
+  })
+  .catch((reason) => {
+    console.log('ens failed!')
+    console.error(reason)
+    resultField.innerText = 'There was a problem: ' + reason.message
+  })
+
+})
